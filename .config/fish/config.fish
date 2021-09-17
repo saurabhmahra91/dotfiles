1a40f31478b2124be6dd3ef171d2c92eae958a80
Saurabh Mahra <sourabhmahra91@gmail.com>
2021-09-17 11:27:00 +0530

    first init

---
+++ .config/fish/config.fish
@@ -0,0 +1,13 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias vim="nvim"
+alias v="nvim"
+
+
+ alias ls='exa -al --color=always --group-directories-first'
+ alias ll='exa -l --color=always --group-directories-first'
+ alias la='exa -a --color=always --group-directories-first'
+ alias lt='exa -aT --color=always --group-directories-first'
+ alias l.='exa -a | egrep "^\."'
